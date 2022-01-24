25063c41085137997e848932f6832b1f60381c09
Isabel Gutiérrez <gm.isabel@gmail.com>
2022-01-24 10:55:07 +0100

    created core components: navigator & footer created models: Core.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { NavigatorInterface, FooterInterface } from './core/models/Core';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'client';
+  title = 'Star Wars - Json Server';
+  public navigator: NavigatorInterface[] = [
+    {
+      name: "home",
+      link: "/home"
+    },
+    {
+      name: "characters",
+      link: "/characters"
+    },
+    {
+      name: "movies",
+      link: "/movies"
+    },
+    {
+      name: "about",
+      link: "/about"
+    }
+  ];
+  public footer: FooterInterface = {
+    creator: "Isabel Gutiérrez",
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CharactersComponent } from './pages/characters/characters.component';
+import { FooterComponent } from './core/components/footer/footer.component';
+import { NavigatorComponent } from './core/components/navigator/navigator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharactersComponent,
+    FooterComponent,
+    NavigatorComponent
   ],
   imports: [
     BrowserModule,
